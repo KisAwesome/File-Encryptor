ac8d9cd46e461acfac5e8b1d5f4f69bdb5de77cf
KisAwesome <cool676rock@gmail.com>
2023-07-29 17:31:11 +0300

    Code refactoring

--- decrypt.py
+++ decrypt.py
@@ -1,16 +1,15 @@
+#!/usr/bin/env python3
+import zono.colorlogger
+import zono.zonocrypt
+import zono.workers
 import argparse
 import shutil
 import sys
-import zono.zonocrypt
 import os
-import zipfile
-import zono.workers
-import zono.colorlogger
 import time
+import zipfile
 
 crypt = zono.zonocrypt.zonocrypt()
-
-HASHED_KEY = None
 
 
 def _divide_chunks(l, n):
@@ -28,30 +27,33 @@ def log(message, func=zono.colorlogger.log, *args, **kwargs):
         func(message, *args, **kwargs)
 
 
-def unzip(path, ext_to):
+def unzip(path):
     with zipfile.ZipFile(path, "r") as ZIP:
         ZIP.extractall()
 
 
 def parse_key(opts):
-    global HASHED_KEY
-
-    STRING_KEY = opts.key
+    string_key = opts.key
     if opts.hash:
-        HASHED_KEY = crypt.hashing_function(STRING_KEY, salt=b"")
+        hashed_key = crypt.hashing_function(string_key, salt=b"")
         return
 
-    if crypt.check_valid_key(STRING_KEY.encode("utf-8")):
-        HASHED_KEY = STRING_KEY
+    if crypt.check_valid_key(string_key.encode("utf-8")):
+        hashed_key = string_key
     else:
-        HASHED_KEY = crypt.hashing_function(STRING_KEY, salt=b"")
+        hashed_key = crypt.hashing_function(string_key, salt=b"")
+
+    return hashed_key
 
 
-def _decrypt(enc, key):
-    return crypt.decrypt_raw(enc, key)
+def _decrypt(enc, key, worker):
+    try:
+        return crypt.decrypt_raw(enc, key)
+    except zono.zonocrypt.IncorrectDecryptionKey:
+        worker.stop()
 
 
-def decrypt_folder(opts):
+def decrypt_folder(opts, hashed_key):
     split_encrypted = opts.file_bytes.split(b"&$&")
     log("Split encrypted bytes")
     worker = zono.workers.ProgressWorkload(
@@ -59,30 +61,39 @@ def decrypt_folder(opts):
     )
 
     log("Starting threads")
-    decrypted_list = worker.run(split_encrypted, _decrypt, HASHED_KEY)
+    decrypted_list = worker.run(split_encrypted, _decrypt, hashed_key)
+    if isinstance(decrypted_list, zono.workers.StoppedWorker):
+        zono.colorlogger.error(
+            "Chunk decryption failed: incorrect key or corrupted file"
+        )
+        return sys.exit()
     log("Threads completed, decrypted chunks")
     file_bytes = b"".join(decrypted_list)
     os.mkdir("temp.encrypted")
     with open("temp.encrypted/temp.zip", "wb") as f:
         f.write(file_bytes)
 
-    log("Written file")
-    unzip("temp.encrypted/temp.zip", os.getcwd())
+    log("Wrote to file")
+    unzip("temp.encrypted/temp.zip")
     log("Unzipped folder successfully")
     print(f"Decrypted file to {opts.filename}")
     log(f"time taken: {time.time()-opts.start_time}s", print)
     shutil.rmtree("temp.encrypted")
 
 
-def decrypt_file(opts):
+def decrypt_file(opts, hashed_key):
     split_encrypted = opts.file_bytes.split(b"&$&")
     log("Split encrypted bytes")
     worker = zono.workers.ProgressWorkload(
         opts.max_threads, ordered_return=True, tqdm_opts=dict(unit="chunks")
     )
-
     log("Starting threads")
-    decrypted_list = worker.run(split_encrypted, _decrypt, HASHED_KEY)
+    decrypted_list = worker.run(split_encrypted, _decrypt, hashed_key, worker)
+    if isinstance(decrypted_list, zono.workers.StoppedWorker):
+        zono.colorlogger.error(
+            "Chunk decryption failed: incorrect key or corrupted file"
+        )
+        return sys.exit()
     log("Threads completed, decrypted chunks")
     file_bytes = b"".join(decrypted_list)
     with open(opts.output_file, "wb") as f:
@@ -91,28 +102,31 @@ def decrypt_file(opts):
     log(f"time taken: {time.time()-opts.start_time}s", print)
 
 
-def decrypt_archived_file(opts):
+def decrypt_archived_file(opts, hashed_key):
     split_encrypted = opts.file_bytes.split(b"&$&")
     log("Split encrypted bytes")
     worker = zono.workers.ProgressWorkload(
         opts.max_threads, ordered_return=True, tqdm_opts=dict(unit="chunks")
     )
-
     log("Starting threads")
-    decrypted_list = worker.run(split_encrypted, _decrypt, HASHED_KEY)
+    decrypted_list = worker.run(split_encrypted, _decrypt, hashed_key, worker)
+    if isinstance(decrypted_list, zono.workers.StoppedWorker):
+        zono.colorlogger.error(
+            "Chunk decryption failed: incorrect key or corrupted file"
+        )
+        return sys.exit()
     log("Threads completed, decrypted chunks")
     file_bytes = b"".join(decrypted_list)
     log("File decrypted")
     with open("temp_file.zip", "wb") as f:
         f.write(file_bytes)
 
-    unzip("temp_file.zip", opts.output_file)
+    unzip("temp_file.zip")
     log("Unzipped file")
     zono.colorlogger.major_log("File decrypted successfully")
 
 
-def decrypt(opts):
-    global HASHED_KEY
+def decrypt(opts, hashed_key):
     opts.start_time = time.time()
     with open(opts.file, "rb") as f:
         file_bytes_enc = f.read()
@@ -133,7 +147,7 @@ def decrypt(opts):
     )
 
     try:
-        filetype_dec = crypt.decrypt(filetype_enc, HASHED_KEY)
+        filetype_dec = crypt.decrypt(filetype_enc, hashed_key)
         log("Decrypted file type")
 
     except zono.zonocrypt.IncorrectDecryptionKey:
@@ -147,59 +161,69 @@ def decrypt(opts):
     if "$#ARCHIVED#$" in filetype_dec:
         filetype_dec = filetype_dec.replace("$#ARCHIVED#$", "")
         log(f"Identified file type: Archived file with the name {filetype_dec}")
-        decrypt_archived_file(opts)
+        decrypt_archived_file(opts, hashed_key)
 
     elif "$#FOLDER#$" in filetype_dec:
         filetype_dec = filetype_dec.replace("$#FOLDER#$", "")
         log(f"Identified file type: Folder with the name {filetype_dec}")
-        decrypt_folder(opts)
+        decrypt_folder(opts, hashed_key)
     else:
         log(f"Identified file type: file type: {filetype_dec}")
-        decrypt_file(opts)
+        decrypt_file(opts, hashed_key)
 
 
-pa = argparse.ArgumentParser("File decrypter", description="Decrypts files")
+def parse_args():
+    pa = argparse.ArgumentParser("File decrypter", description="Decrypts files")
+
+    pa.add_argument("file", help="The file that should be encrypted")
+    pa.add_argument("key", help="The key used for decryption")
+
+    pa.add_argument(
+        "--max-threads",
+        help="Sets the maximum number of threads to use for decryption",
+        default=32,
+        type=int,
+    )
+
+    pa.add_argument(
+        "-v", "--verbose", action="store_true", help="Prints extra information"
+    )
+
+    pa.add_argument(
+        "-replace",
+        action="store_true",
+        help="Replace existing file with decrypted version",
+    )
+
+    pa.add_argument("-of", "--output-file", help="Manually set the output file")
+    pa.add_argument(
+        "-i", "--ignorewarnings", action="store_true", help="Ignores all warnings"
+    )
+
+    pa.add_argument(
+        "--hash",
+        action="store_true",
+        help="Hash the key regardless of whether it is already valid",
+    )
+
+    opts = pa.parse_args()
+    return opts, pa
 
 
-pa.add_argument("file", help="The file that should be encrypted")
-pa.add_argument("key", help="The key used for decryption")
+def main():
+    global VERBOSE
+    opts, pa = parse_args()
+    if not os.path.exists(opts.file):
+        pa.error("Input file does not exist")
 
-pa.add_argument(
-    "--max-threads",
-    help="Sets the maximum number of threads to use for decryption",
-    default=32,
-    type=int,
-)
+    if opts.output_file:
+        if os.path.isdir(opts.output_file):
+            pa.error("Cannot save to a directory")
+    VERBOSE = opts.verbose
 
-
-pa.add_argument("-v", "--verbose", action="store_true", help="Prints extra information")
-
-pa.add_argument(
-    "-replace", action="store_true", help="Replace existing file with decrypted version"
-)
-
-pa.add_argument("-of", "--output-file", help="Manually set the output file")
-pa.add_argument(
-    "-i", "--ignorewarnings", action="store_true", help="Ignores all warnings"
-)
-
-pa.add_argument(
-    "--hash",
-    action="store_true",
-    help="Hash the key regardless of whether it is already valid",
-)
-
-sys.argv.pop(0)
-
-opts = pa.parse_args(sys.argv)
+    hashed_key = parse_key(opts)
+    decrypt(opts, hashed_key)
 
 
-if not os.path.exists(opts.file):
-    pa.error("Input file does not exist")
-
-
-VERBOSE = opts.verbose
-
-parse_key(opts)
-
-decrypt(opts)
+if __name__ == "__main__":
+    main()
---
+++ encrypt.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python3
+import argparse
+import shutil
+import sys
+import zono.zonocrypt
+from english_words import get_english_words_set
+import random
+import os
+import zipfile
+import subprocess
+import zono.workers
+import zono.colorlogger
+import time
+
+
+english_words_set = list(get_english_words_set(["web2"], lower=True))
+
+crypt = zono.zonocrypt.zonocrypt()
+VERBOSE = False
+windows = sys.platform == "win32"
+
+
+def _divide_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+def chunks(l, n):
+    return list(_divide_chunks(l, n))
+
+
+def log(message, func=zono.colorlogger.log, *args, **kwargs):
+    global VERBOSE
+    if VERBOSE:
+        func(message, *args, **kwargs)
+
+
+def hide_dir(Dir):
+    if windows:
+        subprocess.check_call(["attrib", "+H", Dir])
+    else:
+        subprocess.check_call(["chflags", "hidden", Dir])
+
+
+def zip_dir(path, zip_path):
+    zf = zipfile.ZipFile(f"{zip_path}.zip", "w", zipfile.ZIP_DEFLATED)
+
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            zf.write(os.path.join(root, file_name))
+
+
+def write_zip(path, zip_path):
+    zf = zipfile.ZipFile(f"{zip_path}", "w", zipfile.ZIP_DEFLATED)
+    zf.write(path)
+    zf.close()
+
+
+def unzip(path):
+    with zipfile.ZipFile(path, "r") as ZIP:
+        ZIP.extractall()
+
+
+def get_random_word():
+    return random.choice(english_words_set)
+
+
+def get_random_string():
+    _type = random.randint(1, 5)
+    if _type == 1:
+        return f"{get_random_word()}{random.randint(0,99)}"
+    elif _type == 2:
+        return f"{get_random_word()}_{random.randint(0,99)}"
+    elif _type == 3:
+        return f"{get_random_word()}_{get_random_word()}_{random.randint(0,99)}"
+    elif _type == 4:
+        return f"{get_random_word()}-{get_random_word()}-{random.randint(0,99)}"
+    elif _type == 5:
+        return f"{get_random_word()}-{get_random_word()}"
+
+
+def parse_key(opts):
+    if opts.random_key:
+        string_key = get_random_string()
+        hashed_key = crypt.hashing_function(string_key, salt=b"")
+
+    else:
+        string_key = opts.key
+        if opts.hash:
+            hashed_key = crypt.hashing_function(string_key, salt=b"")
+            return
+
+        if crypt.check_valid_key(string_key.encode("utf-8")):
+            hashed_key = string_key
+        else:
+            hashed_key = crypt.hashing_function(string_key, salt=b"")
+
+    return (string_key, hashed_key)
+
+
+def _encrypt(fbytes, key):
+    return crypt.encrypt_bytes(fbytes, key)
+
+
+def encrypt_folder(opts, hashed_key, string_key):
+    opts.output_file = opts.output_file or opts.file + ".encrypted"
+    start_time = time.time()
+    file = "temps.encrypted/file_zip"
+    os.makedirs("temps.encrypted")
+    zip_dir(opts.file, file)
+    log("Zipped folder")
+
+    with open(file + ".zip", "rb") as f:
+        file_bytes = f.read()
+
+    log("Loaded file")
+
+    split_file_bytes = chunks(file_bytes, opts.chunk_size)
+    log("Split folder into chunks")
+    log("Starting threads")
+    worker = zono.workers.ProgressWorkload(
+        opts.max_threads, ordered_return=True, tqdm_opts=dict(unit="chunks")
+    )
+
+    encrypted_chunks = worker.run(split_file_bytes, _encrypt, hashed_key)
+    log("Encrypted chunks")
+    enc_file_byte = b"&$&".join(encrypted_chunks)
+
+    with open(opts.output_file, "wb") as f:
+        f.write(enc_file_byte)
+
+    enc_input_file = crypt.encrypt(opts.file + "$#FOLDER#$", hashed_key)
+
+    with open(opts.output_file, "a") as f:
+        f.write(f"[{enc_input_file}]")
+
+    zono.colorlogger.major_log("File successfully encrypted")
+    print(f"String key: {string_key}\nHashed key: {hashed_key.decode('utf-8')}")
+    log(f"time taken: {time.time()-start_time}s", print)
+
+    shutil.rmtree("temps.encrypted")
+    if opts.replace:
+        shutil.rmtree(opts.file)
+
+
+def encrypt_file(opts, hashed_key, string_key):
+    input_file = os.path.basename(opts.file)
+    input_file_enc = input_file
+    opts.output_file = opts.output_file or input_file + ".encrypted"
+    start_time = time.time()
+    if opts.archive:
+        file = "temps.encrypted/file_zip.zip"
+        os.makedirs("temps.encrypted")
+        write_zip(opts.file, file)
+        log("Zipped file")
+        opts.file = file
+        input_file_enc += "$#ARCHIVED#$"
+
+    with open(opts.file, "rb") as file:
+        file_bytes = file.read()
+
+    log("Loaded file")
+
+    split_file_bytes = chunks(file_bytes, opts.chunk_size)
+    log("Split file into chunks")
+    log("Starting threads")
+    worker = zono.workers.ProgressWorkload(
+        opts.max_threads, ordered_return=True, tqdm_opts=dict(unit="chunks")
+    )
+
+    encrypted_chunks = worker.run(split_file_bytes, _encrypt, hashed_key)
+    log("Encrypted chunks")
+    enc_file_byte = b"&$&".join(encrypted_chunks)
+
+    with open(opts.output_file, "wb") as f:
+        f.write(enc_file_byte)
+
+    enc_input_file = crypt.encrypt(input_file_enc, hashed_key)
+
+    with open(opts.output_file, "a") as f:
+        f.write(f"[{enc_input_file}]")
+
+    zono.colorlogger.major_log("File successfully encrypted")
+    print(f"String key: {string_key}\nHashed key: {hashed_key.decode('utf-8')}")
+    log(f"time taken: {time.time()-start_time}s", print)
+    if opts.archive:
+        shutil.rmtree("temps.encrypted")
+    if opts.replace:
+        os.remove(opts.file)
+
+
+def parse_args():
+    pa = argparse.ArgumentParser(prog="File Encrypter")
+
+    pa.add_argument("file", help="The file that should be encrypted")
+
+    key_gen_group = pa.add_mutually_exclusive_group()
+
+    key_gen_group.add_argument("-key", help="The key that is used to encrypt the file")
+
+    pa.add_argument(
+        "--hash",
+        action="store_true",
+        help="Hash the key regardless of whether it is already valid",
+    )
+
+    key_gen_group.add_argument(
+        "-rk", "--random-key", action="store_true", help="Generates a random random key"
+    )
+
+    pa.add_argument(
+        "-archive",
+        action="store_true",
+        help="Compresses the input file before encryption",
+    )
+    pa.add_argument(
+        "-replace",
+        action="store_true",
+        help="Replace existing file with encrypted version",
+    )
+
+    pa.add_argument(
+        "-i", "--ignorewarnings", action="store_true", help="Ignores all warnings"
+    )
+
+    pa.add_argument("-of", "--output-file", help="Manually set the output file")
+
+    pa.add_argument(
+        "--hashing-function",
+        help="Runs the inputed string through the hashing function",
+        default=False,
+        action="store_true",
+    )
+
+    pa.add_argument(
+        "--max-threads",
+        help="Sets the maximum number of threads to use for encryption",
+        default=32,
+        type=int,
+    )
+
+    pa.add_argument(
+        "--chunk-size",
+        help="Sets the chunk size for encryption",
+        default=32768,
+        type=int,
+    )
+
+    pa.add_argument(
+        "-v", "--verbose", action="store_true", help="Prints extra information"
+    )
+    opts = pa.parse_args()
+    return opts, pa
+
+
+def main():
+    global VERBOSE
+    opts, pa = parse_args()
+    if opts.hashing_function:
+        hashed = crypt.hashing_function(opts.hashing_function, salt=b"").decode("utf-8")
+        print(f"The hash for {opts.hashing_function} is {hashed}")
+        return
+    if not os.path.exists(opts.file):
+        return pa.error("Input file does not exist")
+    if not any([opts.random_key, opts.key]):
+        print("No key provided a random key will be used")
+        opts.random_key = True
+
+    VERBOSE = opts.verbose
+    string_key, hashed_key = parse_key(opts) # type: ignore
+
+    if os.path.exists("temps.encrypted"):
+        shutil.rmtree("temps.encrypted")
+    if os.path.isdir(opts.file):
+        encrypt_folder(opts, hashed_key, string_key)
+
+    else:
+        encrypt_file(opts, hashed_key, string_key)
+
+
+if __name__ == "__main__":
+    main()
